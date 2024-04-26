54dd78ffaa44c4824605d6267bc63d2c0aaf0a17
Ali Haider Swccho <swccho263@gmail.com>
2024-04-26 19:30:36 +0600

    [master]-next-is-file-colocation

--- front-end/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ front-end/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,12 @@
 export default function ReviewDetails({params} : {
-    params : {reviewId: string}
+    params : {
+        productId: string,
+        reviewId: string
+    }
 }){
     return (
         <>
-            <h1>Review Info {params.reviewId}</h1>
+            <h1>Product Info {params.productId} Review Info {params.reviewId}</h1>
         </>
     )
 }
