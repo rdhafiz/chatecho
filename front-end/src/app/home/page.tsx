90b7fe3d6b41723b6d60fe02bf90299de115474f
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2024-05-11 18:00:39 +0600

    ui update

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import Image from "next/image";
+import BannerImage from "../../../public/home/chat-1.png"
 
 function Page() {
     return (
-        <div>
-            {/*Header start*/}
-            <div className="container-fluid">
-                <div className="container">
-                    <div className="flex justify-between items-center">
-                        <h1 className="text-4xl font-bold">Chat<span className="text-theme">Echo</span></h1>
+        <main>
+
+            <div className="container mt-20">
+                <div className="grid grid-cols-2">
+                    <div className="">
+                        <h1 className="text-7xl leading-[90px] font-bold mb-7"><span
+                            className="text-theme">Connetct </span> with <br/>
+                            your circle in <br/> a fun way!</h1>
+                        <p className="mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+                            deleniti est facilis
+                            maiores omnis quam quibusdam sequi similique soluta temporibus?</p>
+
+                        <button
+                            className="px-5 w-[150px] py-3 rounded-full bg-theme text-white hover:bg-sky-600 transition-all duration-300">Get
+                            app
+                        </button>
+                    </div>
+
+                    <div className="grid place-items-center">
+                        <Image src={BannerImage} alt={"bannerImage"}/>
                     </div>
                 </div>
+                
+                <div className="bg-white p-10 rounded-xl shadow bg-gradient-to-r from-red-100 to-orange-200 mt-10"></div>
             </div>
-            {/*Header end  */}
-        </div>
+        </main>
     );
 }
 
