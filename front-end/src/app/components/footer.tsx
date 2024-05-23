8e7d520be12822ff16a954b34a9d134420d1606d
Ahmed Zobayer <a.zobayer@3dvi.com>
2024-05-23 10:31:47 +0600

    ui update

--- front-end/src/app/components/footer.tsx
+++ front-end/src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="container-fluid bg-gradient-to-r from-sky-100 to-indigo-300 ">
-            <div className="container lg:px-10 p-10">
+            <div className="container lg:px-10 px-10 pt-10 pb-5">
                 <div className="grid grid-cols-2">
                     <div className="">
                         <h1 className="text-2xl font-bold mb-10">Chat<span className="text-theme">Echo</span></h1>
@@ -29,7 +29,8 @@ function Footer() {
             </div>
 
             <div className="container p-5 text-center">©2024 All copyrights reserved to
-                <a className="font-bold text-theme" href="https://redishketch.com"> Redishketch</a></div>
+                <a className="font-bold text-theme" target="_blank" href="https://redishketch.com"> Redishketch</a>
+            </div>
         </footer>
     );
 }
--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -9,42 +9,54 @@ import msgBubbleIcon from "../../../public/home/msg-bubble.svg"
 import userIcon from "../../../public/home/user.svg"
 import globeIcon from "../../../public/home/globe.svg"
 import boltIcon from "../../../public/home/bolt.svg"
+import Blob1 from "../../../public/home/blob-1.svg"
 
 function Page() {
     return (
         <main>
 
-            <div className="container mt-20">
-                <div className="grid md:grid-cols-2">
-                    <div className="md:mb-0 mb-5">
-                        <h1 className="lg:text-7xl sm:text-5xl text-3xl lg:leading-[90px] sm:leading-[70px] font-bold mb-7"><span
-                            className="text-theme">Connetct </span> with <br/>
-                            your circle in <br/> a fun way!</h1>
-                        <p className="mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-                            deleniti est facilis
-                            maiores omnis quam quibusdam sequi similique soluta temporibus?</p>
+            <div className="container-fluid relative">
+                <div className="w-[400px] h-[400px] absolute bg-gradient-to-r from-sky-100 to-indigo-300 rounded-full z-0 left-[-50px] top-[-50px]"></div>
+            {/*    <div className="absolute end-0 top-0">
+                    <Image src={Blob1} alt="blob-1"></Image>
+                </div>*/}
+                <div className="container mt-20 z-1 relative">
+                    <div className="grid md:grid-cols-2">
+                        <div className="md:mb-0 mb-5">
+                            <h1 className="lg:text-7xl sm:text-5xl text-3xl lg:leading-[90px] sm:leading-[70px] font-bold mb-7"><span
+                                className="text-theme">Connetct </span> with <br/>
+                                your circle in <br/> a fun way!</h1>
+                            <p className="mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+                                deleniti est facilis
+                                maiores omnis quam quibusdam sequi similique soluta temporibus?</p>
 
-                        <button
-                            className="px-5 w-[150px] py-3 rounded-full bg-theme text-white hover:bg-sky-600 transition-all duration-300">Get
-                            app
-                        </button>
-                    </div>
+                            <button
+                                className="px-5 w-[150px] py-3 rounded-full bg-theme text-white hover:bg-sky-600 transition-all duration-300">Get
+                                app
+                            </button>
+                        </div>
 
-                    <div className="grid place-items-center">
-                        <Image src={BannerImage} alt={"bannerImage"}/>
+                        <div className="grid place-items-center">
+                            <Image src={BannerImage} alt={"bannerImage"}/>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="container">
 
                 {/*Features banner start*/}
                 <div
                     className="bg-white sm:py-10 lg:px-20 sm:px-10 p-5 rounded-2xl shadow bg-gradient-to-r from-sky-100 to-indigo-300 mt-10 mb-10">
 
                     <h1 className="md:text-3xl text-2xl font-bold mb-10">Highlighted features</h1>
-                    <div className="grid md:grid-cols-4 sm:grid-cols-2 lg:gap-7 gap-5  place-items-center max-w-7xl mx-auto">
+                    <div
+                        className="grid md:grid-cols-4 sm:grid-cols-2 lg:gap-7 gap-5  place-items-center max-w-7xl mx-auto">
                         <div className="grid place-items-center text-center group w-full">
                             <div
                                 className="lg:w-[200px] lg:h-[200px] sm:w-[120px] sm:h-[120px] h-[120px] w-full grid place-items-center rounded-[50px] shadow-2xl hover:-translate-y-3 transition-all duration-300 bg-amber-300">
-                                <Image className="lg:w-[100px] w-[60px] lg:h-[100px] h-[60px]" src={PaperPlane} alt={"paper-plane"}/>
+                                <Image className="lg:w-[100px] w-[60px] lg:h-[100px] h-[60px]" src={PaperPlane}
+                                       alt={"paper-plane"}/>
                             </div>
 
                             <div
@@ -154,6 +166,7 @@ function Page() {
 
                 </div>
                 {/*Why choose us end  */}
+
 
             </div>
         </main>
